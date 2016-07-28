617a78defeaddae28ced0844b0e5baa06584b011
nick3499 <indiessance@gmail.com>
2016-07-28 17:55:42 -0500

    Revised Code
    The numeric increments in the combine functions, the `X` and `Y` labels and the `main` `title()` to make it my own code.

--- graph_03.R
+++ graph_03.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env R
 
-X <- c(1, 2, 3, 4, 5, 6, 7, 8)
-Y <- c(2, 6, 7, 10, 17, 19, 23, 29)
-plot(X, Y, pch = 16) 
-plot(X, Y, type = "o", col = "red", xlab = "MY X LABEL", ylab = "MY Y LABEL") 
-title(main = "PLOT 3", font.main = 2, col.main = "blue")
+X <- c(1, 5.5, 10, 14.5, 19, 23.5, 28, 32.5)
+Y <- c(2, 6.5, 11, 15.5, 20, 24.5, 29, 33.5)
+plot(X, Y, pch = 16)
+plot(X, Y, type = "o", col = "red", xlab = "X", ylab = "Y")
+title(main = "Upward Trend", font.main = 2, col.main = "blue")

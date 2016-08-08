721a0235320e74aeff55af2b95a4d85919e75781
nick3499 <indiessance@gmail.com>
2016-08-07 22:18:59 -0500

    Delete graph_03.R

--- graph_03.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env R
-
-X <- c(1, 5.5, 10, 14.5, 19, 23.5, 28, 32.5)
-Y <- c(2, 6.5, 11, 15.5, 20, 24.5, 29, 33.5)
-plot(X, Y, pch = 16)
-plot(X, Y, type = "o", col = "red", xlab = "X", ylab = "Y")
-title(main = "Upward Trend", font.main = 2, col.main = "blue")
